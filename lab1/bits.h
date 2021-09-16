aa2b452115bcdc7cd1ae873034c8ba786bc74d8f
Joshua Rain <1014054551@qq.com>
2021-09-16 03:18:27 -0400

    lab1 first 6 functions

---
+++ lab1/bits.h
@@ -0,0 +1,25 @@
+//2
+long copyLSB(long);
+long test_copyLSB(long);
+long distinctNegation(long);
+long test_distinctNegation(long);
+long getByte(long, long);
+long test_getByte(long, long);
+long anyEvenBit(long);
+long test_anyEvenBit(long);
+//3
+long conditional(long, long, long);
+long test_conditional(long, long, long);
+long subtractionOK(long, long);
+long test_subtractionOK(long, long);
+long isLessOrEqual(long, long);
+long test_isLessOrEqual(long, long);
+long bitMask(long, long);
+long test_bitMask(long, long);
+//4
+long trueThreeFourths(long);
+long test_trueThreeFourths(long);
+long bitCount(long);
+long test_bitCount(long);
+long logicalNeg(long);
+long test_logicalNeg(long);
---
+++ lab1/decl.c
@@ -0,0 +1,53 @@
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define TMin LONG_MIN
+#define TMax LONG_MAX
+
+#include "bits.h"
+#include "btest.h"
+
+test_rec test_set[] = {
+//2
+ {"copyLSB", (funct_t) copyLSB, (funct_t) test_copyLSB, 1,
+    "! ~ & ^ | + << >>", 5, 2,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+ {"distinctNegation", (funct_t) distinctNegation, (funct_t) test_distinctNegation, 1, "! ~ & ^ | +", 5, 2,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+ {"getByte", (funct_t) getByte, (funct_t) test_getByte, 2,
+    "! ~ & ^ | + << >>", 6, 2,
+  {{TMin, TMax},{0,7},{TMin,TMax}}},
+ {"anyEvenBit", (funct_t) anyEvenBit, (funct_t) test_anyEvenBit, 1,
+    "! ~ & ^ | + << >>", 14, 2,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+//3
+ {"conditional", (funct_t) conditional, (funct_t) test_conditional, 3, "! ~ & ^ | << >>", 16, 3,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+ {"subtractionOK", (funct_t) subtractionOK, (funct_t) test_subtractionOK, 2,
+    "! ~ & ^ | + << >>", 20, 3,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+ {"isLessOrEqual", (funct_t) isLessOrEqual, (funct_t) test_isLessOrEqual, 2,
+    "! ~ & ^ | + << >>", 24, 3,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+ {"bitMask", (funct_t) bitMask, (funct_t) test_bitMask, 2,
+    "! ~ & ^ | + << >>", 16, 3,
+  {{0, 63},{0,63},{TMin,TMax}}},
+//4
+ {"trueThreeFourths", (funct_t) trueThreeFourths, (funct_t) test_trueThreeFourths, 1,
+    "! ~ & ^ | + << >>", 20, 4,
+  {{TMin,TMax},{TMin,TMax},{TMin,TMax}}},
+ {"bitCount", (funct_t) bitCount, (funct_t) test_bitCount, 1, "! ~ & ^ | + << >>", 50, 4,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+ {"logicalNeg", (funct_t) logicalNeg, (funct_t) test_logicalNeg, 1,
+    "~ & ^ | + << >>", 12, 4,
+  {{TMin, TMax},{TMin,TMax},{TMin,TMax}}},
+{
+    "", NULL, NULL, 0, "", 0, 0, {
+        {0, 0}, {0, 0}, {
+            0, 0
+        }
+    }
+}
+}
+;
